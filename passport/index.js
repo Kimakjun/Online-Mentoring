7fb85d84037466c44aadec6e0c9abd3015934b8a
dkd7142@naver,com <dkd7142@naver.com>
2021-01-01 21:07:29 +0900

    Feature #noissue : LRU 캐시 적용

--- passport/index.js
+++ passport/index.js
@@ -3,6 +3,9 @@ const kakao = require('./kakaoStrategy');
 
 const {User} = require('../models');
 
+const userList = [];
+const MAX_SIZE = 10;
+
 module.exports = (passport) => {
 
     passport.serializeUser((user, done) => {
@@ -10,10 +13,24 @@ module.exports = (passport) => {
     })
 
     passport.deserializeUser(async (id, done) => {
-        console.log(id);
-        const user = await User.findOne({where : {id}});
-        done(null, user);
+        let deletedIndex = '';
 
+        const exUser = userList.filter((user, i) =>{
+            if(user.id === id){
+                deletedIndex = i;
+                return true;
+            }
+        })[0];  
+   
+        if(exUser){
+            userList.push(userList.splice(deletedIndex, 1)[0]);
+            done(null, exUser);
+        }else{
+            const user = await User.findOne({where : {id}});
+            if(userList.length >= MAX_SIZE) userList.shift();
+            userList.push(user);
+            done(null, user);
+        }
     })
  
     local(passport);
